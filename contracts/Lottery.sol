e040e95b51faa1e36881ab542c1b545e512d16f8
Iosismu <evanpark333@gmail.com>
2022-02-22 16:07:14 +0900

    dapp

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,18 +1,67 @@
 pragma solidity >=0.4.22 <0.9.0;
-
 contract Lottery {
+    struct BetInfo {
+        uint256 answerBlockNumber;
+        address payable bettor;
+        byte challenges; // 0xab
+    }
     // public으로 만들게 되면 자동으로 getter를 만들어 준다.
     // 그래서 Smart Contract 외부에서 변수(owner) 값을 바로 확인 가능.
     address public owner;
-
+    uint256 private _tail;
+    uint256 private _head;
+    mapping(uint256 => BetInfo) private _bets;
+    uint256 internal constant BLOCK_LIMIT = 256;
+    uint256 internal constant BET_BLOCK_INTERNAL = 3;
+    uint256 internal constant BET_AMOUNT = 5 * 10**15;
+    uint256 private _pot;
+    event BET(uint256 index, address bettor, uint256 amount, byte challenges, uint256 answerBlockNumber);
     // Smart Contracts가 실행,배포가 될 떄 가장먼저 실행 되는 함수!!
     constructor() public {
         // 배포가 될 떄 보낸사름으로 owner를 하겠다.
         // msg.sender는 Smart Contracts 자체에서 제공 해 주는 글로벌 변수
         owner = msg.sender;
     }
-
-    function getSomeValue() public pure returns (uint256 value) {
-        return 5;
+    function getPot() public view returns (uint256 pot) {
+        return _pot;
+    }
+    // Bet
+    /**
+    * @dev 배팅을 한다. 유저는 0.005 ETF를 보내야 하고, 배팅용 1 byte 글자를 보낸다.
+    * 큐에 저장된 베팅 정보는 이후 distribute 함수에서 해결된다.
+    * @param challenges 유저가 베팅하는 글자
+    * @return 함수가 잘 수행되었는지 확인해주는 bool 값
+    */
+    function bet(byte challenges) public payable returns (bool result) {
+      // Check the proper ether is sent
+      require(msg.value == BET_AMOUNT, "Not enough ETH");
+      // Push bet to the queue
+      require(pushBet(challenges), "Fail to add a new Bet Info");
+      // Emit event
+      emit BET(_tail - 1, msg.sender, msg.value, challenges, block.number + BET_BLOCK_INTERNAL);
+      return true;
+    }
+    // save the bet to the queue
+    // Distribute
+      // check the answer
+    function getBetInfo(uint256 index) public view returns (uint256 answerBlockNumber, address bettor, byte challenges) {
+      BetInfo memory b = _bets[index];
+      answerBlockNumber = b.answerBlockNumber;
+      bettor = b.bettor;
+      challenges = b.challenges;
+    }
+    function pushBet(byte challenges) internal returns (bool) {
+       BetInfo memory b;
+       b.bettor = msg.sender;
+       b.answerBlockNumber = block.number + BET_BLOCK_INTERNAL;
+       b.challenges = challenges;
+       _bets[_tail] = b;
+       _tail++;
+       return true;
+    }
+    function popBet(uint256 index) internal returns (bool) {
+      // delete는 일정량의 Gas를 돌려준다. 그래서 사용하지 않는 값은 delete해주는게 좋다. 
+      delete _bets[index];
+      return true;
     }
 }
--- test/lottery.test.js
+++ test/lottery.test.js
@@ -1,19 +1,42 @@
 const Lottery = artifacts.require("Lottery");
-
+const assertRevert = require('./asserRevert');
+const expectEvent = require('./expectEvent');
 contract('Lottery', function ([deployer, user1, user2]) {
   let lottery;
+  let betAmount = 5 * 10 ** 15;
+  let bet_block_interval = 3;
   beforeEach(async () => {
-    console.log('Before each')
     lottery = await Lottery.new();
   })
-
-  it('Basic test', async () => {
-    console.log('Basic test')
-    let owner = await lottery.owner();
-    let value = await lottery.getSomeValue();
-
-    console.log(`owner : ${owner}`);
-    console.log(`value : ${value}`);
-    assert.equal(value, 5)
+  it('getPot should return current pot', async () => {
+    let pot = await lottery.getPot();
+    assert.equal(pot, 0)
+  })
+  describe.only("Bet", function () {
+    it('should fail when the bet money is not 0.005 ETH', async () => {
+      // Fail transaction
+      await assertRevert(lottery.bet("0xab", { from: user1, value: 4000000000000000 }))
+      // transaction object {chainId, value, to, from, gas(Limit), gasPrice}
+    })
+    it('should put the bet to the bet queue with 1 bet', async () => {
+      // Bet
+      let receipt = await lottery.bet("0xab", { from: user1, value: betAmount })
+      console.log(receipt);
+      let pot = await lottery.getPot();
+      assert.equal(pot, 0)
+      // Check contractBalance == 0.005
+      let contractBalance = await web3.eth.getBalance(lottery.address);
+      assert.equal(contractBalance, betAmount)
+      // Check bet info
+      let currentBlockNumber = await web3.eth.getBlockNumber();
+      let bet = await lottery.getBetInfo(0);
+      assert.equal(bet.answerBlockNumber, currentBlockNumber + bet_block_interval);
+      assert.equal(bet.bettor, user1);
+      assert.equal(bet.challenges, '0xab');
+      
+      // Check logic
+      // console.log(receipt);
+      await expectEvent.inLogs(receipt.logs, 'BET');
+    })
   })
 });
